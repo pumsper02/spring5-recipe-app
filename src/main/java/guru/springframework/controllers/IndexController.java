bc78e6ab41f01c115369c2b66b8c18983fa1c1be
pumsper <pumsper02@gmail.com>
2023-01-16 21:15:47 +0700

    use repository for get data from database

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,12 +1,35 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import guru.springframework.domain.Category;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Tablespoon");
+
+        System.out.println("Cat Id is:"+categoryOptional.get().getId());
+        System.out.println("UnitOfMeasure Id is:"+unitOfMeasure.get().getId());
+
+
         return "index";
     }
+
+
 }
